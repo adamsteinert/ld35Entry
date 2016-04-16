0c93aab082abdab2089696657a7bbc2ba14b3b09
Adam Steinert <alphajulietsierra@gmail.com>
2016-04-16 01:22:50 -0500

    Testing controls and shape shifting

--- src/states/StageState.js
+++ src/states/StageState.js
@@ -2,33 +2,75 @@ import * as util from 'Utilities';
 import {game} from 'index';
 import {bgColors} from 'Constants'
 
+
+
 class StageState extends Phaser.State {
   preload() {
-    game.load.tilemap('tileMap', 'assets/tilemap/TestMap.json', null, Phaser.Tilemap.TILED_JSON);
-    game.load.image("tiles", "assets/tilemap/TileSet_Master.png")
+    game.load.tilemap('tileMap', 'assets/tilemap/Tilemap_Master.json', null, Phaser.Tilemap.TILED_JSON);
+    game.load.image("tiles", "assets/tilemap/TileSet_Master.png");
+    game.load.spritesheet('playerSpriteSheet', 'assets/sprites/player_bounds.png', 64, 64);
+  }
+
+
+  onShapeShift() {
+    let p = this.player
+    if(p.frame === 0)
+    {
+      //stick
+      p.frame = 6
+      p.body.setSize(26, 61, 0, 0)
+      p.angle -= 90
+    }
+    else if(p.frame === 6)
+    {
+      p.frame = 13
+      p.body.setSize(53, 63, 6, 0)
+      p.y -= 39
+      p.angle += 90
+    }
+    else {
+      // squid
+      p.frame = 0
+      p.body.setSize(61, 24, 2, 0)
+    }
   }
 
 	create() {
+    this.touching = false;
+    game.time.advancedTiming = true;
 		util.trace('StageState::create')
-    game.world.setBounds(0, 0, 1024, 1024);
-    this.game.physics.startSystem(Phaser.Physics.ARCADE);
 
+    //game.world.setBounds(0, 0, 1024, 1024);
+    game.world.setBounds(0, 0, 4800, 1600);
     this.stage.backgroundColor = 0x2c2b2b;
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+    game.physics.arcade.gravity.y = 400;
+
+    // CONTROLS
+    this.cursor = game.input.keyboard.createCursorKeys();
+    this.S = this.game.input.keyboard.addKey(Phaser.Keyboard.S);
+    this.S.onDown.add(this.onShapeShift, this);
+
+    //TILES
+    //the first parameter is the tileset name as specified in Tiled, the second is the key to the asset
     this.map = this.game.add.tilemap('tileMap');
+		this.map.addTilesetImage('MasterTileset', 'tiles');
+    this.blockedLayer = this.map.createLayer("TileArea")
+		this.map.setCollisionBetween(0,20, true, "TileArea");
+    //this.map.setCollision(14, true, "TileArea");
+    //this.testLayer = this.map.createLayer("Foo")
+    //this.map.setCollisionBetween(3,5, true, "Foo");
 
-		//the first parameter is the tileset name as specified in Tiled, the second is the key to the asset
-		this.map.addTilesetImage('TileSet_Master', 'tiles');
-    this.blockedLayer = this.map.createLayer("TileLayer")
-		this.map.setCollisionBetween(1,5, true, "TileLayer");
 
-    this.player = this.game.add.sprite(100, 100, 'player');
-		//  We need to enable physics on the player
+    // PLAYER
+    this.player = this.game.add.sprite(100, 1440, 'playerSpriteSheet')
+    this.player.anchor.setTo(0.5, 0.5);
+
 		game.physics.arcade.enable(this.player);
-
-		//  Player physics properties. Give the little guy a slight bounce.
-		this.player.body.bounce.y = 0.2;
-		this.player.body.gravity.y = 300;
 		this.player.body.collideWorldBounds = true;
+    this.player.body.bounce.y = 0.2;
+
+    this.onShapeShift()
 
     game.camera.follow(this.player);
 		//let style = { font: "32px Arial", fill: "#ff0044", align: "center"};
@@ -36,9 +78,50 @@ class StageState extends Phaser.State {
 		//text.anchor.set(0.5);
 	}
 
+
 	update() {
-      game.physics.arcade.collide(this.player, this.blockedLayer);
+    let p = this.player;
+    let cursor = this.cursor;
+    let climbing = false;
+
+    game.physics.arcade.collide(this.player, this.blockedLayer);
+
+    if(p.frame === 6 && (p.body.blocked.left || p.body.blocked.right)) {
+      p.body.allowGravity = false
+      climbing = true;
+    }
+    else {
+      this.touching = false
+      p.body.allowGravity = true
+    }
+
+    p.body.velocity.x = 0;
+
+    if (cursor.up.isDown && climbing) {
+      p.body.velocity.y = -150;
+    }
+    else if(cursor.down.isDown && climbing) {
+      p.body.velocity.y = 150;
+    }
+    if (cursor.left.isDown)
+    {
+        p.body.velocity.x = -150;
+    }
+    else if (cursor.right.isDown)
+    {
+        p.body.velocity.x = 150;
+    }
+
+
+
 	}
+
+  render() {
+    game.debug.text(game.time.fps || '--', 2, 32, "#00ff00");
+    game.debug.text(this.touching , 2, 64, "#00ff00");
+    game.debug.cameraInfo(game.camera, 32, 32);
+    game.debug.spriteCoords(this.player, 32, 150);
+  }
 }
 
 export default StageState;
